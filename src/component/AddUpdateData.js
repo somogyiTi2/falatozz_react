439e3535254e4bea27b1ff256feab15cf1a57446
Somogyi Tibor <somogyi.tibor1997@gmail.com>
2023-03-27 13:45:37 +0200

    Update make chack

--- src/component/AddUpdateData.js
+++ src/component/AddUpdateData.js
@@ -1,50 +1,118 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-export const AddUpdateData = (props) => {
-
+const AddUpdateData = (props) => {
+    //megoldható useStatel 
     const [enteredName, setEnteredName] = useState(props.update.name);
-    const [eneteredDescription, setEneteredDescription] = useState(props.update.description);
+    const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
     const [enteredPiece, setEnteredPiece] = useState(props.update.piece);
-    
-    function UpdateName(event) {
+    const [enteredPieceTouched, setEnteredPieceTouched] = useState(false);
+
+    const [enteredDescription, setEnteredDescription] = useState(props.update.description);
+    const [enteredDescriptionTouched, setEnteredDescriptionTouched] = useState(false);
+
+
+    const enteredNameIsValid = enteredName.trim() !== '';
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+
+    const enteredPieceIsValid = enteredPiece.trim() !== '';
+    const pieceInputIsInvalid = !enteredPieceIsValid && enteredPieceTouched;
+
+    const enteredDescriptionIsValid = enteredDescription.trim() !== '';
+    const descriptionInputIsInvalid = !enteredDescriptionIsValid && enteredDescriptionTouched;
+
+    let formIsValid = false;
+    //minden változó igaz e, pl életkor név minden igaz e
+    if (enteredNameIsValid && enteredPieceIsValid && enteredDescriptionIsValid) {
+        formIsValid = true;
+    }
+
+
+    const nameInputChangeHandler = event => {
         setEnteredName(event.target.value);
     };
-
-    function UpdateDescription(event) {
-        setEneteredDescription(event.target.value);
-    };
-
-    function UpdatePiece(event) {
+    const pieceInputChangeHandler = event => {
         setEnteredPiece(event.target.value);
     };
 
-    const updateHandeler = (event) => {
+    const descriptionInputChangeHandler = event => {
+        setEnteredDescription(event.target.value);
+    };
+
+    //Blur funkció feladata, ha nem írunk be semmit csak tovább kattintuk hibát dob fel.
+    const nameInputBlurHandler = event => {
+        setEnteredNameTouched(true);
+    };
+    const pieceInputBlurHandler = event => {
+        setEnteredPieceTouched(true);
+    };
+
+    const descriptionInputBlurHandler = event => {
+        setEnteredDescriptionTouched(true);
+    };
+
+    const forSubmissionHandler = event => {
         event.preventDefault();
+        //ez állítja azt,be hogy már műkködött e egyszer a validáció
+        if (!enteredNameIsValid) {
+            return;
+        }
+
         props.UpdateHandler({
             id: props.update.id,
             name: enteredName,
-            description: eneteredDescription,
+            description: enteredDescription,
             piece: enteredPiece,
         })
+        setEnteredNameTouched(false);
+        setEnteredPieceTouched(false);
+        setEnteredDescriptionTouched(false);
     }
 
-    return (
-        <form>
-            <p>
-                <layer>Frissítendő neve:  <input type="text" onChange={UpdateName}
-                    value={enteredName} /></layer>
-            </p>
-            <p>
-                <layer>Frissítendő leírása: <input type="text" onChange={UpdateDescription} value={eneteredDescription} /></layer>
-            </p> <p>
-                <layer>Frissítendő darab: <input type="number" onChange={UpdatePiece} value={enteredPiece} /></layer>
-            </p>
-            <button onClick={props.onShow}>Mégse</button>
-    
-            <button onClick={updateHandeler}>Frissít</button>
+    const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+    const PieceenteredName = pieceInputIsInvalid ? 'form-control invalid' : 'form-control';
+    const DescriptionenteredName = descriptionInputIsInvalid ? 'form-control invalid' : 'form-control';
 
+    return (
+        <form onSubmit={forSubmissionHandler}>
+            <div className={nameInputClasses}>
+                <label htmlFor='name'>Néve:</label>
+                <input type='text'
+
+                    onChange={nameInputChangeHandler}
+                    onBlur={nameInputBlurHandler}
+
+                    value={enteredName} />
+            </div>
+            {nameInputIsInvalid && <p className="error-text">Üres a név!</p>}
+
+            <div className={DescriptionenteredName}>
+                <label htmlFor='name'>Leírás:</label>
+                <input type='text'
+
+                    onChange={descriptionInputChangeHandler}
+                    onBlur={descriptionInputBlurHandler}
+                    value={enteredDescription}
+                />
+            </div>
+            {descriptionInputIsInvalid && <p className="error-text">Üres a leírás!</p>}
+      
+            <div className={PieceenteredName}>
+                <label htmlFor='name'>Darab:</label>
+                <input type='number'
+                    onChange={pieceInputChangeHandler}
+                    onBlur={pieceInputBlurHandler}
+                    value={enteredPiece}
+
+                />
+            </div>
+            {pieceInputIsInvalid && <p className="error-text">Üres a darab!</p>}
+            <div className="form-actions">
+                <button disabled={!formIsValid}>Küldés</button>
+            </div>
         </form>
-    )
-}
+    );
+};
 
 export default AddUpdateData;
